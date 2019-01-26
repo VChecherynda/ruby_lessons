c5ff361a1edd33f952aba1014d29109ec28f920d
Vadim Checherynda <checherinda.vadym@gmail.com>
2019-01-27 00:19:21 +0200

    add const for query

--- companies_and_employees.rb
+++ companies_and_employees.rb
@@ -1,10 +1,13 @@
 require 'mysql2'
 
-client = Mysql2::Client.new(:host => "localhost", :port => '3306', :username => "root", :database => "comapnies_and_employees")
-results = client.query("SELECT companies.name, employees.name, employees.salary 
+QUERY = "SELECT companies.name AS name_of_the_company, employees.name, employees.salary 
                         FROM companies
                         INNER JOIN employees
-                        ON companies.id = employees.company_id")
+                        ON companies.id = employees.company_id"
+
+
+client = Mysql2::Client.new(:host => "localhost", :port => '3306', :username => "root", :database => "comapnies_and_employees")
+results = client.query(QUERY)
 
 results.each do |row|
   puts row
