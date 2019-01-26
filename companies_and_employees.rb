9244b746a73a113ca08579aca32e5ddecbf9b25f
Vadim Checherynda <checherinda.vadym@gmail.com>
2019-01-27 00:12:32 +0200

    Add inner join  for 2 tables

--- companies_and_employees.rb
+++ companies_and_employees.rb
@@ -1,7 +1,12 @@
 require 'mysql2'
 
 client = Mysql2::Client.new(:host => "localhost", :port => '3306', :username => "root", :database => "comapnies_and_employees")
-results = client.query("SELECT * FROM employees")
+results = client.query("SELECT companies.name, employees.name, employees.salary 
+                        FROM companies
+                        INNER JOIN employees
+                        ON companies.id = employees.company_id")
 
-puts results
+results.each do |row|
+  puts row
+end
 
