34ebef94eb6e2866961e90ba240e0efc21778d5d
Vadim Checherynda <checherinda.vadym@gmail.com>
2019-01-26 23:50:09 +0200

    fix for db name

--- companies_and_employees.rb
+++ companies_and_employees.rb
@@ -1,6 +1,6 @@
 require 'mysql2'
 
-client = Mysql2::Client.new(:host => "localhost", :port => '3306', :username => "root", :database => "`comapnies_and_employees`")
+client = Mysql2::Client.new(:host => "localhost", :port => '3306', :username => "root", :database => "comapnies_and_employees")
 results = client.query("SELECT * FROM employees")
 
 puts results
