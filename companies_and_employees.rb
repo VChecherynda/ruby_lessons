787383e6b828357e0d62d5dcfd8ecaebfae6efef
Vadim Checherynda <checherinda.vadym@gmail.com>
2019-01-27 20:36:16 +0200

    Proper format for output string

--- companies_and_employees.rb
+++ companies_and_employees.rb
@@ -1,33 +1,26 @@
 require 'mysql2'
 require 'pry'
 
-QUERY = "SELECT companies.name AS name_of_the_company, employees.name, employees.salary 
-                        FROM companies
-                        INNER JOIN employees
-                        ON companies.id = employees.company_id"
-
+QUERY = "SELECT 
+           companies.id AS company_id,
+           companies.name,
+           employees.id,
+         SUM(employees.salary),
+         COUNT(employees.id)
+         FROM companies 
+         INNER JOIN employees 
+         ON companies.id = employees.company_id 
+         GROUP BY company_id"
 
 client = Mysql2::Client.new(:host => "localhost", :port => '3306', :username => "root", :database => "comapnies_and_employees")
 results = client.query(QUERY)
 
-@prev_name_of_the_company 
-@new_table = []
+@f = File.open('emplyees_and_salaries.txt','w')
 
-results.each_with_index do |row,index|
+@f.printf("%-16s %-16s %-16s %1s", "Company name:", "Total employess:", "Total salaries:", "\n\n")
 
-  current_name_of_the_company = row["name_of_the_company"]
-
-  if @prev_name_of_the_company != current_name_of_the_company
-
-    @prev_name_of_the_company = current_name_of_the_company
-
-    @new_table << {
-      "name_of_the_company" => current_name_of_the_company,
-      "total_number_of_employees" => @new_table[index] ? @new_table[index]["total_number_of_employees"] + 1 : 0,
-      "total_salary" => @new_table[index] ? @new_table[index]["total_salary"] + 1 : 0
-    }
-  end
-
+results.each do |row|
+  @f.printf("%-16s %-16s %-16s %1s", row["name"], row["SUM(employees.salary)"], row["COUNT(employees.id)"], "\n")
 end
 
-puts @new_table
+@f.close
