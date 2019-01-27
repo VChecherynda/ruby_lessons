4e6dfd182101d9ab13bfad152e742ad2df3a5a02
Vadim Checherynda <checherinda.vadym@gmail.com>
2019-01-27 11:33:21 +0200

    current stage

--- companies_and_employees.rb
+++ companies_and_employees.rb
@@ -1,4 +1,5 @@
 require 'mysql2'
+require 'pry'
 
 QUERY = "SELECT companies.name AS name_of_the_company, employees.name, employees.salary 
                         FROM companies
@@ -9,7 +10,24 @@ QUERY = "SELECT companies.name AS name_of_the_company, employees.name, employees
 client = Mysql2::Client.new(:host => "localhost", :port => '3306', :username => "root", :database => "comapnies_and_employees")
 results = client.query(QUERY)
 
-results.each do |row|
-  puts row
+@prev_name_of_the_company 
+@new_table = []
+
+results.each_with_index do |row,index|
+
+  current_name_of_the_company = row["name_of_the_company"]
+
+  if @prev_name_of_the_company != current_name_of_the_company
+
+    @prev_name_of_the_company = current_name_of_the_company
+
+    @new_table << {
+      "name_of_the_company" => current_name_of_the_company,
+      "total_number_of_employees" => @new_table[index] ? @new_table[index]["total_number_of_employees"] + 1 : 0,
+      "total_salary" => @new_table[index] ? @new_table[index]["total_salary"] + 1 : 0
+    }
+  end
+
 end
 
+puts @new_table
